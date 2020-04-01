178713417cd97a199d9f8ab55aff5c84559b50ba
jakobln <62882745+jakobln@users.noreply.github.com>
2020-04-01 12:26:54 +0200

    Tilføjet property "By"
    Opdateret toString() med denne property.

--- Program.cs
+++ Program.cs
@@ -8,9 +8,9 @@ namespace GitExercise
         {
             //Console.WriteLine("Hello World!");
 
-            myClass minklasse = new myClass("Jakob", "Mariendalsvej 6b", 3600);
-            myClass klasse2 = new myClass("Peter", "Solvej 15", 3600);
-            myClass klasse3 = new myClass("Jens", "Roskildevej 13", 3600);
+            myClass minklasse = new myClass("Jakob", "Mariendalsvej 6b", 3600, "Frederikssund");
+            myClass klasse2 = new myClass("Peter", "Solvej 15", 3600, "Frederikssund");
+            myClass klasse3 = new myClass("Jens", "Roskildevej 13", 3600, "Frederikssund");
             System.Console.WriteLine(minklasse);
             System.Console.WriteLine(klasse2);
             System.Console.WriteLine(klasse3);
--- myClass.cs
+++ myClass.cs
@@ -5,6 +5,7 @@ namespace GitExercise
         private string _navn;
         private string _adresse;
         private int _postnr;
+        private string _by;
 
         public string Navn 
         {
@@ -18,17 +19,22 @@ namespace GitExercise
         {
             get {return _postnr;}
         }
+        public string By
+        {
+            get {return _by;}
+        }
 
-        public myClass(string Navn, string Adresse, int Postnr)
+        public myClass(string Navn, string Adresse, int Postnr, string By)
         {
             _navn = Navn;
             _adresse = Adresse;
             _postnr = Postnr;
+            _by = By;
         }
 
         public override string ToString()
         {
-            return Navn + " " + Adresse + " " + Postnr;
+            return Navn + " " + Adresse + " " + Postnr + " " + By;
         }
     }
 }
