add85f42add4af1966ec646f3a3fb62120284a4d
jakobln <62882745+jakobln@users.noreply.github.com>
2020-04-01 10:45:36 +0200

    Tilføjet properties
    Navn, Adresse og Postnr

--- myClass.cs
+++ myClass.cs
@@ -2,6 +2,8 @@ namespace GitExercise
 {
     class myClass
     {
-        
+        public string Navn {get; set;}
+        public string Adresse {get; set;}
+        public int Postnr {get; set;}
     }
 }
