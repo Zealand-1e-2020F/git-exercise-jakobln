6c3a48498ab58eca7c04b9d658a360126982ec53
jakobln <62882745+jakobln@users.noreply.github.com>
2020-04-01 10:51:53 +0200

    Tilføjet constructor
    Constructor myClass med Navn, Adresse og Postnr

--- myClass.cs
+++ myClass.cs
@@ -2,8 +2,19 @@ namespace GitExercise
 {
     class myClass
     {
+        private string navn;
+        private string adresse;
+        private int postnr;
+
         public string Navn {get; set;}
         public string Adresse {get; set;}
         public int Postnr {get; set;}
+
+        public myClass(string Navn, string Adresse, int Postnr)
+        {
+            Navn = navn;
+            Adresse = adresse;
+            postnr = Postnr;
+        }
     }
 }
